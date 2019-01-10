86462e18672bafb71b6f19636b0d9b411527ff48
charm <charm@HANSOL>
2019-01-10 20:10:41 +0900

    충돌나야지

--- gitTest/src/git/test/Main.java
+++ gitTest/src/git/test/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("첫번쨰 수정내용");
 		String name = "유병승";
+		System.out.println("asdf");
 		System.out.println("이거 충돌나야해");
 		int age =19;
 	}
