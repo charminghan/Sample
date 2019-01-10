1988e26c6915a213985def68bc3fd5edf2c888ab
charm <charm@HANSOL>
2019-01-10 20:05:35 +0900

    충돌나야지~~!!!

--- gitTest/src/git/test/Main.java
+++ gitTest/src/git/test/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("첫번쨰 수정내용");
-		System.out.println("이거 충돌나니?");
+		System.out.println("이거 충돌나야해");
 	}
 
 }
